cc642b9dc88027f17cb29a8ddada734f9b6010fe
julio-front-end-dev <contato.julinhoalmeida@outlook.com>
2026-05-05 19:08:37 -0300

    Update config.example.php

--- config.example.php
+++ config.example.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * MODELO DE CONFIGURAÇÃO - FACILITA!
- * Renomeie para config.php e preencha os dados abaixo.
- */
+
 session_start();
 
 $host = 'localhost';
@@ -12,4 +9,4 @@ $db   = 'facilita_db';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-// [Repetir aqui a mesma lógica de Cadastro e Login do config.php]
+
